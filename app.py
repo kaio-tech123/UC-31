b59dce8b926132f8bce33fefa63e82bb973e07bd
ciro gomemes <vkaio2022@gmail.com>
2026-05-20 17:08:48 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template('index.html')
-   
+
+@app.route('/cardapio')
+def cardapio():
+    return render_template('cardapio.html')
+
+@app.route('/lanche/<nome>')
+def lanche(nome):
+    lanches_validos = ['pizza', 'hamburguer', 'batata', 'milkshake']
+    
+    encontrado = nome.lower() in lanches_validos
+    
+    return render_template('lanche.html', nome=nome, encontrado=encontrado)
+
+@app.route('/pedidos')
+def pedidos():
+    return render_template('pedidos.html')
+
+@app.route('/cliente')
+def cliente(nome, city):
+    
+    return render_template('cliente.html')
 
 @app.route('/contato')
 def contato():
-    nome = "Joãozinho"
-    return render_template('index.html', title= 'Página inicial', nome=nome)
-
-@app.route('/usuario')
-def usuario():
-    usuario = {'nome:', 'João', 'email:', 'joaozinho@gmail.com'}
-    return render_template('index.html', title= 'Página inicial', usuario=usuario)
+    return render_template('contato.html')
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(debug=True)

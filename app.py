3773252da57516e85450d3c61053569e9153064c
ciro gomemes <vkaio2022@gmail.com>
2026-07-15 17:07:49 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,35 +1,44 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+usuario_cadastrado = None
+senha_cadastrada = None
 
-@app.route('/cardapio')
-def cardapio():
-    return render_template('cardapio.html')
-
-@app.route('/lanche/<nome>')
-def lanche(nome):
-    lanches_validos = ['pizza', 'hamburguer', 'batata', 'milkshake']
+@app.route('/', methods=['GET', 'POST'])
+def cadastro():
+    global usuario_cadastrado, senha_cadastrada
     
-    encontrado = nome.lower() in lanches_validos
+    if request.method == 'POST':
+        nome = request.form['nome']
+        senha = request.form['senha']
+        
+        senha_cadastrada = generate_password_hash(senha)
+        usuario_cadastrado = nome
+        
+        return redirect(url_for('login'))
+        
+    return render_template('cadastro.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    global usuario_cadastrado, senha_cadastrada
+    erro = None
     
-    return render_template('lanche.html', nome=nome, encontrado=encontrado)
-
-@app.route('/pedidos')
-def pedidos():
-    return render_template('pedidos.html')
-
-@app.route('/cliente')
-def cliente(nome, city):
-    
-    return render_template('cliente.html')
-
-@app.route('/contato')
-def contato():
-    return render_template('contato.html')
+    if request.method == 'POST':
+        nome_tentativa = request.form['nome']
+        senha_tentativa = request.form['senha']
+        
+        if usuario_cadastrado and nome_tentativa == usuario_cadastrado:
+            if check_password_hash(senha_cadastrada, senha_tentativa):
+                return render_template('boas_vindas.html', nome=usuario_cadastrado)
+            else:
+                erro = "Senha incorreta!"
+        else:
+            erro = "Usuário não encontrado!"
+            
+    return render_template('login.html', erro=erro)
 
 if __name__ == '__main__':
     app.run(debug=True)
